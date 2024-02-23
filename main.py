468f14abf6c51ac0088ace19990c20eaa361c2e4
magprone <magpronem@gmail.com>
2024-02-23 13:41:19 +0300

    GET method created

--- main.py
+++ main.py
@@ -20,5 +20,12 @@ def add_company():
     }
     return jsonify({param: companies[autoincrement][param] for param in ['id', 'name', 'budget', 'spent_budget']})
 
+
+@app.route('/api/partners/<int:id>', methods=['GET'])  # pyright: ignore
+def get_company(id):
+    global companies
+    return jsonify({param: companies[id][param] for param in ['id', 'name', 'budget', 'spent_budget', 'is_stopped']})
+
+
 if __name__ == '__main__':
   app.run(host='localhost', port=8080, debug=True)
